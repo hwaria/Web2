2532070b9afd41085e1de3be76a421e64c9597ef
lialee <46589495+hwaria@users.noreply.github.com>
2019-05-21 17:25:53 +0900

    Update Node.js

--- Node.js
+++ Node.js
@@ -58,7 +58,7 @@ app.listen(3000);
 -	줄을 바꿀때마다 역슬래쉬를 쓸 수 있지만 쓰면 코드상 줄바꿈이 되는 것이지 문자열에서 줄바꿈이 되는 것은 아님
 -	역슬래쉬n (\n)을 쓰면 줄바꿈이 됨
 -	기능은 하지만 변수를 쓰기 위해 산술 연산자를 쓰는 것이 불편하니까.. 최신 문법으로 ‘template literal’ 개발됨
--	Literal이란 정보를 표현하는 방법, 기호. Esc버튼 밑에 작은 따옴표 비슷하게 생긴 것을 씀 
+-	Literal이란 정보를 표현하는 방법, 기호. Esc버튼 밑에 작은 따옴표 비슷하게 생긴 것을 씀 (`) 
 var letter = `Dear ${name}    // 변수를 쓸 때는 달러{변수 이름}
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ${name} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ${1+1} Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. ${name}`;
 console.log(letter);
@@ -83,17 +83,121 @@ var _url = request.url;
 var queryData = url.parse(_url, true).query; // -> {id: ‘HTML’}
 console.log(queryData.id); -> HTML
   if (_url == "/") {
-    url = "/index.html";
+    _url = "/index.html";
   }
   if (_url == "/favicon.ico") {
-    response.writeHead(404);
-    response.end();
-    return;
+       return response.writeHead(404);
   }
   response.writeHead(200);
   response.end(queryData.id); //사용자가 접속한 url에 따라 적합한 웹페이지를 읽어주는 역할이었으므로, url에 입력된 값을 이용하여 해당 데이터를 전송
 });
 app.listen(3000);
 
--	url 안에 있는 값을 추출해야 함 - (parsing query string in node.js)
+-	url 안에 있는 값을 추출해야 함 - (parsing query string in node.js) -> url.parse(request.url, true).query
+-	response.end() 안에 있었던 “fs.readFIleSynd(__dirname + _url” 부분은 사용한 접속한 링크에 따라서 적합한 파일을 읽어주는 역할을 했었음. 이 부분을 queryData.id (HTML, CSS 등)로 바꿈
 -	위에 코드 추가  
+
+11.	App – 동적인 웹페이지 만들기
+-	쿼리스트링에 따라 다르게 동작하는 애플리케이션을 만들었으니 이제 쿼리스트링에 따라 다른, 완성된 웹페이지를 표현해보기
+-	“1.html” 내용을 복사해서 main.js내에 var template에 넣어줌
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+
+var app = http.createServer(function(request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var title = queryData.id; // 좀 더 직관적인 이름 주기 
+  if (_url == "/") {
+    title = "Welcome"; // 최상위 페이지에 들어왔을 때 title은 welcome으로.
+  }
+  if (_url == "/favicon.ico") {
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  var template = ` // 이것이 template literal을 쓰는 방법
+<!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title> // 내용이 바뀔 부분은 이런 식으로 역동적으로 바뀌게 해줌
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <ol>
+    <li><a href="/?id=HTML">HTML</a></li> //링크 클릭했을 때 쿼리스트링을 html로 바꿔주기
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <h2>${title}</h2>
+  <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+  <img src="coding.jpg" width="100%">
+  </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+  </p>
+</body>
+</html>
+`;
+  response.end(template);
+});
+app.listen(3000);
+
+-	이제 우리는 정보를 역동적으로 프로그래밍적으로 생성할 수 있게 됨 -> main.js에서 html태그만 바꾸면 그 태그가 적용된 모든 웹 페이지의 html태그를 동시에 바꿀 수 있음 
+-	이제 제목은 동적으로 바꿀 수 있지만 본문은 아직 정적임: 이제 각각의 파일에 페이지에 맞는 본문만을 저장했다가 사용자의 요청이 들어왔을 때 요청에 해당되는 파일을 읽어서 내용을 바꿀 수 있으면 좋겠네
+
+12.	Node.js의 파일 읽기 기능 
+-	Node.js에서 파일을 다루는 방법을 배워보자
+-	파일 읽기: 노드의 모듈 중에 파일을 읽어내는 ‘fs’ 모듈 사용
+-	‘Nodejs’라는 폴더내에 ‘fileread.js’, ‘sample.text’파일 만들어서 fileread.js에 아래 코드 작성
+var fs = require("fs");
+fs.readFile("sample.txt", 'utf8', function(err, data) {
+  console.log(data);
+});
+-	파일 실행시킬 때는 cd src/nodejs 로 간 후에 node fildread.js를 통해 파일 읽기
+-	‘utf8’을 넣어야 텍스트가 제대로 읽어짐
+
+13.	App – 파일을 이용해 본문 구현
+-	이제 파일에 본문을 저장하고. 노드의 파일읽기 기능 (fs.readFile)을 이용해서 본문을 생성하는 방법 살펴보쟈
+-	새로운 폴더 (data)를 만들고 그 안에 html, css, javascript 본문 내용을 복붙
+-	Main.js에서 본문에 해당하는 부분을 쿼리스트링값에 따라서 데이터 폴더에 잇는 적당한 파일을 읽어서 내용을 치환해 줌
+-	 fs.readFile(`data/${queryData.id}` 우리가 읽어들일 파일: 쿼리스트링에 따라 파일명이 생성
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+
+var app = http.createServer(function(request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var title = queryData.id;
+  if (_url == "/") {
+    title = "Welcome";
+  }
+  if (_url == "/favicon.ico") {
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
+    var template = `
+  <!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <ol>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <h2>${title}</h2>
+  <p>${description}</p>
+</body>
+</html>
+`;
+    response.end(template);
+  });
+});
+app.listen(3000);
+-	
+-	아직까지 데이터가 있는 폴더로 경로 지정이 이뤄지지 않아서 (?id=의 값을 정의하지 않아서) 제목, 본문이 undefined로 나옴
